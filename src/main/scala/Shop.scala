b4e31bdaeb3a4066ad3d46087e9829e3a43f8717
Administrator <arkadiusk.kowalski.qa.com>
2017-06-19 14:41:35 +0100

    uml creation

---
+++ src/main/scala/Shop.scala
@@ -0,0 +1,9 @@
+/**
+  * Created by Administrator on 19/06/2017.
+  */
+class Shop {
+  def hey:Unit={
+    //
+  }
+
+}
--- src/main/scala/SummarySaleRecord.scala
+++ src/main/scala/SummarySaleRecord.scala
@@ -1,9 +1,6 @@
 /**
   * Created by Administrator on 19/06/2017.
   */
-class Test {
-  def hey:Unit={
-    //
-  }
+class SummarySaleRecord {
 
 }
