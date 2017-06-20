cc7b832d1a37ce5887263eaab0d526aa9919a488
Fi <feeon@live.com>
2017-06-20 16:13:16 +0100

    Creation of Tests for Employee and Manager using FlatSpec

--- src/test/scala/ManagerSalesViewTest.scala
+++ src/test/scala/ManagerSalesViewTest.scala
@@ -23,20 +23,3 @@ class ManagerSalesViewTest extends FlatSpec {
 
 }
 
-//
-//
-//  info("I want to be able to view a days sales")
-//  info("So that I know how much money the shop has made")
-//
-//  feature ("View days sales")
-//    scenario("Manager can view the days sales")
-//
-//  Given (" That the shop has opened")
-//  val viewTodaysSales = newSalesRecord
-//
-//  When (" The shop has made sales")
-//
-//
-//  Then ("The manager can view the days sales")
-//}
-//
