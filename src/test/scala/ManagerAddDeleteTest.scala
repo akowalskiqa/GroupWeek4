fc5ec680c86d7b365bece074973720b2ac0cd39d
Scott <scott3933@gmail.com>
2017-06-20 16:41:39 +0100

    Fixed manager add/del tests

--- src/test/scala/ManagerAddDeleteTest.scala
+++ src/test/scala/ManagerAddDeleteTest.scala
@@ -5,28 +5,6 @@ import scala.collection.mutable.ArrayBuffer
 /**
   * Created by Administrator on 20/06/2017.
   */
-/*
-class ManagerAddDeleteTest extends FeatureSpec with GivenWhenThen {
-
-  info("As a Manager")
-  info("I want to be able to add or delete a Items to the system")
-  info("So I can keep selling the latest products")
-  info("And ahead of my competitors")
-
-  feature("Adding a Item to the list"){
-    Scenario("Manager adds a new Item to the list of items"){
-      Given("The Item to be added is not in the list already")
-      val Item1 = new Item(id:Int,name:String, price: Double, pointValue:Int)
-      assert()//ItemList doesn't Contains Item1
-
-      When("Item is added")
-      addItemToList(Item1)
-
-      Then("The new item is added onto the list")
-      assert()//ItemList contains Item1
-    }
-  }
-} */
 
 class ManagerAddDeleteTest extends FlatSpec {
 
@@ -34,7 +12,7 @@ class ManagerAddDeleteTest extends FlatSpec {
 
   "As a Manager I want to be able to add a new item to the list" should "Add a new item to the list" in {
     var shop = new Shop()
-    var item1 = new Game(1,"game1")
+    var item1 = new Game(2, "Hitman", 10.0, 10, false)
     var itemlist: ArrayBuffer[Game] = ArrayBuffer()
 
     itemlist += item1 // Item is added into the list
@@ -43,7 +21,7 @@ class ManagerAddDeleteTest extends FlatSpec {
   }
   "As a Manager I want to be able to add that new item to the stock" should "Add a new item to stock" in {
     //    updateStock(Item1.id)
-    var item1 = new Game(1,"game1")
+    var item1 = new Game(2, "Hitman", 10.0, 10, false)
     //   var stock = new Stock(item1.id)
     assert(true) //ItemList contains Item1
   }
@@ -51,7 +29,7 @@ class ManagerAddDeleteTest extends FlatSpec {
 
   "A Manager" should "be able to delete items from the items list" in {
     val shop = new Shop()
-    val whoInvokedThisCall = new Manager(6, "Elliot", 2)
+    val whoInvokedThisCall = new Manager("Elliot",6,2)
 
     if (whoInvokedThisCall.isInstanceOf[Manager]) {
       shop.deleteAnItem(4)
@@ -64,7 +42,7 @@ class ManagerAddDeleteTest extends FlatSpec {
   it should "not delete if user is not a Manager" in {
 
     val shop = new Shop()
-    val whoInvokedThisCall = new FloorStaff(1, "Ryan", 1)
+    val whoInvokedThisCall = new FloorStaff("Ryan",1,2)
 
     if (!whoInvokedThisCall.isInstanceOf[Manager]) {
       shop.deleteAnItem(4)
