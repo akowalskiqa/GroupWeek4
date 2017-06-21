bdc574c3a47a14e10aee17e8a25605f8687d6d50
Fi <feeon@live.com>
2017-06-21 17:32:45 +0100

    Employee Sell products test

--- src/test/scala/EmployeeSellProductsTest.scala
+++ src/test/scala/EmployeeSellProductsTest.scala
@@ -13,18 +13,16 @@ class EmployeeSellProductsTest extends FlatSpec {
   " As an Employee I" can " sell a product to a customer" in {
     var shop = new Shop()
     var item1 = shop.defineAnItem(ItemTypes.Game, "Bonestorm", 10.0, 10)
-    val employee = new FloorStaff("Ryan", shop.idGenerator.uniqueEmployeeId)
+    var shoppingList = Array(item1)
+    var stockList = new Stock()
+    stockList.productQuantity +=(11-> 100)
+    val whoInvokedThisCall = new FloorStaff("Ryan", shop.idGenerator.uniqueEmployeeId)
     val customer = new Customer("Richard", 1234, true, 105)
-    val itemList = new ArrayBuffer[Item]()
+    if (whoInvokedThisCall.isInstanceOf[FloorStaff]) {
 
-
-    assert(shop.sellThis(item1).getItemName() == "Bonestorm")
-
-    itemList+= item1
-    val array = itemList.toArray
-
-    var sales = new SaleRecord(array, 15.75, Date, employee, customer, 10, 1234 )
-
+      shop.sellThis  (shoppingList, stockList)
+      assert(shoppingList.contains(("Bonestorm")))
+    }
 
   }
 }
