7d8c40730b8f9066feb3f4c93bd1198f3b4a73e0
Scott <scott3933@gmail.com>
2017-06-21 10:04:50 +0100

    stuff

--- src/main/scala/Game.scala
+++ src/main/scala/Game.scala
@@ -1,8 +1,16 @@
 /**
   * Created by Administrator on 19/06/2017.
   */
-class Game(override val itemID:Int,override val name:String,override val price:Double,override val pointValue:Int,var preOrder:Boolean) extends Item(itemID,name,price,pointValue) {
+class Game(var itemID: Int, var name: String, var price: Double, var pointValue: Int,var preOrder: Boolean) extends Item() {
   def getPreOrderAvailability():Boolean={preOrder}
   def setPreOrderAvailability(status:Boolean):Unit={preOrder=status}
+
+  override def getItemID(): Int = itemID
+
+  override def getItemName(): String = name
+
+  override def getItemPrice(): Double = price
+
+  override def getItemPointValue(): Int = pointValue
 
 }
--- src/main/scala/Misc.scala
+++ src/main/scala/Misc.scala
@@ -1,6 +1,13 @@
 /**
   * Created by Administrator on 19/06/2017.
   */
-class Misc(override val itemID:Int,override val name:String,override val price:Double,override val pointValue:Int) extends Item(itemID,name,price,pointValue){
+class Misc(var itemID: Int, var name: String, var price: Double, var pointValue: Int) extends Item(){
 
+  override def getItemID(): Int = itemID
+
+  override def getItemName(): String = name
+
+  override def getItemPrice(): Double = price
+
+  override def getItemPointValue(): Int = pointValue
 }
