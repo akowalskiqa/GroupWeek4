894163b7a0d537a25481f241701645c5a25de12c
Administrator <arkadiusk.kowalski.qa.com>
2017-06-21 15:49:08 +0100

    accept payment completed

--- src/main/scala/FloorStaff.scala
+++ src/main/scala/FloorStaff.scala
@@ -15,9 +15,14 @@ class FloorStaff(name:String,val employeeID: Int,var authorityLevel:Int) extends
   def closeShop(shopToClose : Shop,whereToUpdateTheTally:SummarySaleRecord):Unit ={
   shopToClose.closeShop(whereToUpdateTheTally)
   }
-  def sellItem(listOfItemsToSell:Array[Item],customerBuyingTheProducts:Customer,shop:Shop,stock:Stock,whoAmI:FloorStaff,summary:SummarySaleRecord): Unit ={
-    shop.sellThis(listOfItemsToSell,customerBuyingTheProducts,stock,whoAmI,summary:SummarySaleRecord)
+  def sellItem(listOfItemsToSell:Array[Item],customerBuyingTheProducts:Customer,shop:Shop,stock:Stock,whoAmI:FloorStaff,summary:SummarySaleRecord): (Array[Item], Double, Int) ={
+    shop.sellThis(listOfItemsToSell,stock) getOrElse  (Array[Item](), 0, 0)
   }
+//listForPurchaseFinalisation: Array[Item], cost: Double,customerBuyingTheProducts: Customer, whoAmI: FloorStaff, stock: Stock, summary: SummarySaleRecord, paymentWithPoints: Option[Int] = None
+  def finaliseSale(listForPurchaseFinalisation: Array[Item], cost: Double,customerBuyingTheProducts: Customer, whoAmI: FloorStaff, stock: Stock, summary: SummarySaleRecord,shop:Shop, paymentWithPoints: Option[Int] = None): Unit ={
+    shop.acceptPayment(listForPurchaseFinalisation,cost,customerBuyingTheProducts,whoAmI,stock,summary,paymentWithPoints)
+  }
+
   def checkAmountInStockForThisID(whichStock:Stock,itemId:Int): Int ={
     whichStock.getAmountOfProductsForThisID(itemId)
   }
