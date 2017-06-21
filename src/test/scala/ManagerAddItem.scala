6b5743b2266dc81679f53dfd47c6120bd7a1fec3
Scott <scott3933@gmail.com>
2017-06-21 15:09:38 +0100

    Fixing more tests

--- src/test/scala/ManagerAddItem.scala
+++ src/test/scala/ManagerAddItem.scala
@@ -6,21 +6,24 @@ import scala.collection.mutable.ArrayBuffer
   */
 class ManagerAddItem extends FlatSpec {
 
-//    info("As a Manager I want to be able to add a new item to sell")
-//
-//    "As a Manager I want to be able to add a new item to the list" should "Add a new item to the list" in {
-//      var shop = new Shop()
-//      var item1 = new Game
-//      var itemlist: ArrayBuffer[Game] = ArrayBuffer()
-//
-//      itemlist += item1// Item is added into the list
-//      //   createAnItem()
-//      assert(itemlist.last == item1) //Item is generated
-//    }
-//    "As a Manager I want to be able to add that new item to the stock" should "Add a new item to stock" in {
-//      //    updateStock(Item1.id)
-//      var item1 = new Game
-//   //   var stock = new Stock(item1.id)
-//      assert(true)//ItemList contains Item1
-//    }
+  info("As a Manager I want to be able to add a new item to sell")
+
+  "As a Manager I want to be able to add a new item to the list" should "Add a new item to the list" in {
+    var shop = new Shop()
+    var item1 = shop.defineAnItem(ItemTypes.Game, "Mass Effect", 15.0, 10)
+    // = new Game(2, "Hitman", 10.0, 10, false)
+    var itemlist: ArrayBuffer[Item] = ArrayBuffer()
+
+    itemlist += item1 // Item is added into the list
+    //   createAnItem()
+    assert(itemlist.last == item1) //Item is generated
   }
+  "As a Manager I want to be able to add that new item to the stock" should "Add a new item to stock" in {
+    //    updateStock(Item1.id)
+    var shop = new Shop()
+    var item1 = shop.defineAnItem(ItemTypes.Game, "Mass Effect", 15.0, 10)
+    shop.listOfStock += item1.getItemName()
+    //   var stock = new Stock(item1.id)
+    assert(shop.listOfStock != null) //ItemList contains something
+  }
+}
