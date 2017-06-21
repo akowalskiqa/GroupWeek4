6b50db31487f9e28bc5b6e01d5f7171d7016c84a
Scott <scott3933@gmail.com>
2017-06-21 16:18:03 +0100

    Fixing more tests

--- src/test/scala/ManagerSalesViewTest.scala
+++ src/test/scala/ManagerSalesViewTest.scala
@@ -31,15 +31,31 @@ class ManagerSalesViewTest extends FlatSpec {
     //val shop = new Shop
     //var amountOfDailySales = employee.getDailySales(19.06.2017)
     assert(shop.listOfSales.nonEmpty)
+    assert(shop.listOfSales.head.totalCost == 15.75)
 
   }
 
   it should "Allow the manager to view sales from other days" in {
-   // val employee = new Manager
-   // val sales = new SaleRecord
-    //val shop = new Shop
-    //var selectedDateSales = employee.selectedDateSales(15.04.2017)
-    //assert()
+    var shop = new Shop()
+    var item1 = shop.defineAnItem(ItemTypes.Misc, "Monsters Inc",2.50, 5)
+    var item2 = shop.defineAnItem(ItemTypes.Misc, "Terminator",2.50, 5)
+    var item3 = shop.defineAnItem(ItemTypes.Game, "Halo",2.50, 5)
+    val employee1 = new FloorStaff("John", shop.idGenerator.uniqueEmployeeId)
+    val customer = new Customer("Jack", 1234, true, 105)
+    val itemList = new ArrayBuffer[Item]()
+
+    itemList+= item1
+    itemList+= item2
+    itemList+= item3
+    val array = itemList.toArray
+
+    var sales = new SaleRecord(array, 15.75, Date , employee1, customer, 10, 1234 )
+    var sales2 = new SaleRecord(array, 12.25, Date , employee1, customer, 15, 1330 )
+    shop.listOfSales += (sales, sales2)
+
+    assert(shop.listOfSales.nonEmpty)
+    //assert(shop.listOfItems.indexWhere(item => item.getItemPrice() == 12.25))
+
   }
 
 }
