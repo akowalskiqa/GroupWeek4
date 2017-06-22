b26ffb68ab8992eb54cd96e13fdd151a9cc044bd
Scott <scott3933@gmail.com>
2017-06-22 10:21:56 +0100

    Even more working

--- src/test/scala/EmployeeSellProductsTest.scala
+++ src/test/scala/EmployeeSellProductsTest.scala
@@ -1,13 +1,9 @@
-import java.util.Date
-
 import org.scalatest.FlatSpec
-
-import scala.collection.mutable.ArrayBuffer
 
 /**
   * Created by Profile on 20/06/2017.
   */
-
+//Working
 class EmployeeSellProductsTest extends FlatSpec {
 
   " As an Employee I" can " sell a product to a customer" in {
@@ -22,8 +18,26 @@ class EmployeeSellProductsTest extends FlatSpec {
 
       shop.sellThis  (shoppingList, stockList)
       //  assert(shoppingList.exists(item => item.getItemName()=="Bonestorm"))
-      assert(shoppingList.contains(("Bonestorm")))
+      assert(shoppingList.contains(item1))//("Bonestorm")))
     }
 
+  }
+  it should "not sell a product without staff" in{
+    var shop = new Shop()
+    var item1 = shop.defineAnItem(ItemTypes.Game, "Bonestorm", 10.0, 10)
+    var item2 = null
+    var shoppingList = Array(item1)
+    var stockList = new Stock()
+    stockList.productQuantity +=(11-> 100)
+    val whoInvokedThisCall = new Customer("Richard", 1234, true, 105)
+
+    if (whoInvokedThisCall.isInstanceOf[FloorStaff]) {
+
+      shop.sellThis  (shoppingList, stockList)
+      assert(shoppingList.contains(item1))
+    }
+    else{
+      assert(!shoppingList.contains(item2))
+    }
   }
 }
