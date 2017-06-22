dc4db59796a1f8d2a0848c895b956c0ff71d31ef
Scott <scott3933@gmail.com>
2017-06-22 14:26:30 +0100

    Try catch

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,5 @@
+import PersonType.PersonType
+
 /**
   * Created by Administrator on 20/06/2017.
   */
@@ -17,28 +19,38 @@ object Main {
     def login(): Unit = {
       println("Press 1: Manager\nPress 2: Floor Staff\nPress 0: Exit System")
       var scanner = scala.io.StdIn.readLine()
-
-      scanner match {
-        case "0" => sys.exit()
-        case "1" => println("Welcome Manager"); managerMenu()
-        case "2" => println("Welcome Floor Staff"); floorStaffMenu()
-        case _ => println("Error - Incorrect key pressed\nReturned to current page"); login()
+      try {
+        scanner match {
+          case "0" => sys.exit()
+          case "1" => println("Welcome Manager"); managerMenu()
+          case "2" => println("Welcome Floor Staff"); floorStaffMenu()
+          case _ => println("Error - Incorrect key pressed\nReturned to current page"); login()
+        }
       }
+      catch
+        {
+          case _: Throwable => println("Error, Please try again!"); login()
+        }
     }
 
     def floorStaffMenu(): Unit = {
 
       println("Press 1: Transaction\nPress 2: Open Shop\nPress 3: Close Shop\nPress 4: Daily Figures\nPress 5: Log out")
       var scanner = scala.io.StdIn.readLine()
-
-      scanner match {
-        case "1" => println("Transaction Screen"); floorStaffTransaction()
-        case "2" => println("Shop Opened"); shop.openShop() ;floorStaffMenu() // Add Function/
-        case "3" => println("Shop Closed") ;sys.exit()
-        case "4" => println("Daily Figures: ") //Ad Function
-        case "5" => login()
-        case _ => println("Error - Incorrect key pressed\nReturned to current page"); floorStaffMenu()
+      try {
+        scanner match {
+          case "1" => println("Transaction Screen"); floorStaffTransaction()
+          case "2" => println("Shop Opened"); shop.openShop(); floorStaffMenu() // Add Function/
+          case "3" => println("Shop Closed"); sys.exit()
+          case "4" => println("Daily Figures: ") //Ad Function
+          case "5" => login()
+          case _ => println("Error - Incorrect key pressed\nReturned to current page"); floorStaffMenu()
+        }
       }
+      catch
+        {
+          case _: Throwable => println("Error, Please try again!"); floorStaffMenu()
+        }
 
     }
 
@@ -46,30 +58,40 @@ object Main {
       println("Press 1: Add item to basket by item ID\nPress 2: Show basket\nPress 3: Checkout\nPress 4: List stock\nPress 5: Clear basket\n" +
         "Press 6: Remove item from the basket\nPress 0: Go back to menu")
       var scanner = scala.io.StdIn.readLine()
-
-      scanner match {
-        case "0" => floorStaffMenu()
-        case "1" => println("Enter ID for item you adding to basket: "); var scanner = scala.io.StdIn.readLine(); shop.addItemToShoppingBasket(scanner.toInt); floorStaffTransaction()
-        case "2" => println(shop.listOfItemsToSell.mkString("\n")); floorStaffTransaction()
-        case "3" => println("Checkout"); floorStaffTransaction() //shop.sellThis()
-        case "4" => println(shop.listOfItems.mkString("\n")); floorStaffTransaction()
-        case "5" => println("Clearing basket..."); shop.clearShoppingBasket(); floorStaffTransaction()
-        case "6" => println("Enter ID for item you're removing from basket: "); var scanner = scala.io.StdIn.readLine(); shop.removeItemById(scanner.toInt); floorStaffTransaction()
-        case _ => println("Error - Incorrect key pressed\nReturned to current page"); floorStaffTransaction()
+      try {
+        scanner match {
+          case "0" => floorStaffMenu()
+          case "1" => println("Enter ID for item you adding to basket: "); var scanner = scala.io.StdIn.readLine(); shop.addItemToShoppingBasket(scanner.toInt); floorStaffTransaction()
+          case "2" => println(shop.listOfItemsToSell.mkString("\n")); floorStaffTransaction()
+          case "3" => println("Checkout"); floorStaffTransaction() //shop.sellThis()
+          case "4" => println(shop.listOfItems.mkString("\n")); floorStaffTransaction()
+          case "5" => println("Clearing basket..."); shop.clearShoppingBasket(); floorStaffTransaction()
+          case "6" => println("Enter ID for item you're removing from basket: "); var scanner = scala.io.StdIn.readLine(); shop.removeItemById(scanner.toInt); floorStaffTransaction()
+          case _ => println("Error - Incorrect key pressed\nReturned to current page"); floorStaffTransaction()
+        }
       }
+      catch
+        {
+          case _: Throwable => println("Error, Please try again!"); floorStaffTransaction()
+        }
 
     }
 
     def floorStaffCheckout(): Unit = {
       println("Press 1: Pay with cash\nPress 2: Pay with points\nPress 0: Go back to menu")
       var scanner = scala.io.StdIn.readLine()
-
-      scanner match {
-        case "0" => floorStaffMenu()
-        case "1" => println("Pay with cash")
-        case "2" => println("Pay with points")
-        case _ => println("Error - Incorrect key pressed\nReturned to current page"); floorStaffCheckout()
+      try {
+        scanner match {
+          case "0" => floorStaffMenu()
+          case "1" => println("Pay with cash")
+          case "2" => println("Pay with points")
+          case _ => println("Error - Incorrect key pressed\nReturned to current page"); floorStaffCheckout()
+        }
       }
+      catch
+        {
+          case _: Throwable => println("Error, Please try again!"); floorStaffCheckout()
+        }
 
     }
 
@@ -77,72 +99,113 @@ object Main {
     def managerMenu(): Unit = {
       println("Press 1: Stock\nPress 2: Employee\nPress 3: Sales\nPress 4: Log out\nPress 0: Exit system")
       var scanner = scala.io.StdIn.readLine()
-
-      scanner match {
-        case "0" => sys.exit()
-        case "1" => stockManager()
-        case "2" => employeeManager();
-        case "3" => managerSales()
-        case "4" => login()
-        case _ => println("Error - Incorrect key pressed\nReturned to current page"); managerMenu()
+      try {
+        scanner match {
+          case "0" => sys.exit()
+          case "1" => stockManager()
+          case "2" => employeeManager();
+          case "3" => managerSales()
+          case "4" => login()
+          case _ => println("Error - Incorrect key pressed\nReturned to current page"); managerMenu()
+        }
       }
+      catch
+        {
+          case _: Throwable => println("Error, Please try again!"); managerMenu()
+        }
     }
 
     def stockManager(): Unit = {
       println("Press 1: Add item to stock list\nPress 2: Update item details\nPress 3: Delete item\nPress 4: Update quantity\nPress 0: Back to menu")
       var scanner = scala.io.StdIn.readLine()
-
-      scanner match {
-        case "0" => managerMenu()
-        case "1" => println("Enter Item Type: ");var itemType =  scala.io.StdIn.readLine(); println("Enter Item Name: ");var itemName =  scala.io.StdIn.readLine(); stockManager()
-          println("Enter Item Price: ");var itemPrice =  scala.io.StdIn.readLine();println("Enter Amount of points for item: ");var itemPoints =  scala.io.StdIn.readLine(); stockManager()
-          println("Is item for pre-order [True/False]: ");var itemPreorder =  scala.io.StdIn.readLine();//shop.defineAnItem(itemType = itemType, itemName, itemPrice.toDouble, itemPoints.toInt, itemPreorder.toBoolean)
-        case "2" => println("Update item details"); stockManager()
-        case "3" => println("Enter ID for item you're deleting: "); var scanner = scala.io.StdIn.readLine(); shop.deleteAnItem(scanner.toInt); stockManager()
-        case "4" => println("Enter ID for item you're updating stock quantity: "); var itemID = scala.io.StdIn.readLine(); println("Enter new stock quantity number: "); var quantity = scala.io.StdIn.readLine(); shop.updateStockForID(itemID.toInt, quantity.toInt); stockManager()
-        case _ => println("Error - Incorrect key pressed\nReturned to current page"); stockManager()
+      try {
+        scanner match {
+          case "0" => managerMenu()
+          case "1" => println("Enter Item Type: ");
+            var itemType = scala.io.StdIn.readLine();
+            println("Enter Item Name: ");
+            var itemName = scala.io.StdIn.readLine();
+            stockManager()
+            println("Enter Item Price: ");
+            var itemPrice = scala.io.StdIn.readLine();
+            println("Enter Amount of points for item: ");
+            var itemPoints = scala.io.StdIn.readLine();
+            stockManager()
+            println("Is item for pre-order [True/False]: ");
+            var itemPreorder = scala.io.StdIn.readLine(); //shop.defineAnItem(itemType = itemType, itemName, itemPrice.toDouble, itemPoints.toInt, itemPreorder.toBoolean)
+          case "2" => println("Update item details"); stockManager()
+          case "3" => println("Enter ID for item you're deleting: "); var scanner = scala.io.StdIn.readLine(); shop.deleteAnItem(scanner.toInt); stockManager()
+          case "4" => println("Enter ID for item you're updating stock quantity: "); var itemID = scala.io.StdIn.readLine(); println("Enter new stock quantity number: "); var quantity = scala.io.StdIn.readLine(); shop.updateStockForID(itemID.toInt, quantity.toInt); stockManager()
+          case _ => println("Error - Incorrect key pressed\nReturned to current page"); stockManager()
+        }
       }
+      catch
+        {
+          case _: Throwable => println("Error, Please try again!"); stockManager()
+        }
     }
 
     def employeeManager(): Unit = {
       println("Press 1: Add Floor staff\nPress 2: Delete floor staff\nPress 3: View Floor staff Employee details\nPress 4: View Employee listing\nPress 5: Update Employee record\nPress 0: Back to menu")
       var scanner = scala.io.StdIn.readLine()
       var employeeType:PersonType = PersonType.Manager
-
-      scanner match {
-        case "0" => managerMenu()
-        case "1" => println("Enter the name of the floor staff you're adding: ");var floorstaffname = scala.io.StdIn.readLine();println("Enter employee type: \nPress 1: Manager\nPress 2: Floor Staff"); var personType = scala.io.StdIn.readLine() ;
-         personType match {
-           case "1" => employeeType = PersonType.Manager
-           case "2" => employeeType = PersonType.FloorStaff
-           case _ => println("Error"); employeeManager()
-         }
-          println("Enter the floorstaff ID: "); var floorstaffid = scala.io.StdIn.readLine()
-          shop.createAnFloorStaff(shop.defineAnPersonType(employeeType,floorstaffname,floorstaffid.toInt)) ; employeeManager()
-        case "2" => println("Enter the Floor Staff ID that you want to delete: "); var scanner = scala.io.StdIn.readLine(); shop.deleteAnFloorStaff(scanner.toInt); employeeManager()
-        case "3" => println("Enter Floor Staff ID you want to view: "); var scanner = scala.io.StdIn.readLine(); println(" " + shop.readAnFloorStaff(scanner.toInt)); employeeManager()
-        case "4" => println(shop.listOfFLoorStaff.mkString("\n")); employeeManager()
-        case "5" => println("Enter the Employee ID you want to update: "); var employeeid = scala.io.StdIn.readLine();println("Enter employee type: \nPress 1: Manager\nPress 2: Floor Staff"); var personType = scala.io.StdIn.readLine() ;
-          personType match {
-            case "1" => employeeType = PersonType.Manager
-            case "2" => employeeType = PersonType.FloorStaff
-            case _ => println("Error"); employeeManager()
-          } ;println("Enter the updated name for the Employee you wish to update record: ");var employeeNewName = scala.io.StdIn.readLine(); shop.updateAnFloorStaff(employeeid.toInt, employeeType, employeeNewName); employeeManager()
-        case _ => println("Error - Incorrect key pressed\nReturned to current page"); employeeManager()
+      try {
+        scanner match {
+          case "0" => managerMenu()
+          case "1" => println("Enter the name of the floor staff you're adding: ");
+            var floorstaffname = scala.io.StdIn.readLine();
+            println("Enter employee type: \nPress 1: Manager\nPress 2: Floor Staff");
+            var personType = scala.io.StdIn.readLine();
+            personType match {
+              case "1" => employeeType = PersonType.Manager
+              case "2" => employeeType = PersonType.FloorStaff
+              case _ => println("Error"); employeeManager()
+            }
+            println("Enter the floorstaff ID: ");
+            var floorstaffid = scala.io.StdIn.readLine()
+            shop.createAnFloorStaff(shop.defineAnPersonType(employeeType, floorstaffname, floorstaffid.toInt));
+            employeeManager()
+          case "2" => println("Enter the Floor Staff ID that you want to delete: "); var scanner = scala.io.StdIn.readLine(); shop.deleteAnFloorStaff(scanner.toInt); employeeManager()
+          case "3" => println("Enter Floor Staff ID you want to view: "); var scanner = scala.io.StdIn.readLine(); println(" " + shop.readAnFloorStaff(scanner.toInt)); employeeManager()
+          case "4" => println(shop.listOfFLoorStaff.mkString("\n")); employeeManager()
+          case "5" => println("Enter the Employee ID you want to update: ");
+            var employeeid = scala.io.StdIn.readLine();
+            println("Enter employee type: \nPress 1: Manager\nPress 2: Floor Staff");
+            var personType = scala.io.StdIn.readLine();
+            personType match {
+              case "1" => employeeType = PersonType.Manager
+              case "2" => employeeType = PersonType.FloorStaff
+              case _ => println("Error"); employeeManager()
+            };
+            println("Enter the updated name for the Employee you wish to update record: ");
+            var employeeNewName = scala.io.StdIn.readLine();
+            shop.updateAnFloorStaff(employeeid.toInt, employeeType, employeeNewName);
+            employeeManager()
+          case _ => println("Error - Incorrect key pressed\nReturned to current page"); employeeManager()
+        }
       }
+      catch
+        {
+          case _: Throwable => println("Error, Please try again!"); employeeManager()
+        }
     }
 
     def managerSales(): Unit = {
       println("Press 1: Previous figures\nPress 2: Current figures\nPress 3: Future figures\nPress 0: Back to menu")
       var scanner = scala.io.StdIn.readLine()
-
-      scanner match {
-        case "0" => managerMenu()
-        case "1" => println("Previous figures function")
-        case "2" => println("Current figures function")
-        case "3" => println("Future figures function")
-        case _ => println("Error - Incorrect key pressed\nReturned to current page"); managerSales()
+      try {
+        scanner match {
+          case "0" => managerMenu()
+          case "1" => println("Previous figures function")
+          case "2" => println("Current figures function")
+          case "3" => println("Future figures function")
+          case _ => println("Error - Incorrect key pressed\nReturned to current page"); managerSales()
+        }
       }
+      catch
+        {
+          case _: Throwable => println("Error, Please try again!"); managerSales()
+        }
     }
 
   }
