bd1f0dd34d65cc66023c84affcc070d780e247eb
Fi <feeon@live.com>
2017-06-22 12:04:47 +0100

    Employee View Stock test - completed

--- src/test/scala/EmployeeViewStockTest.scala
+++ src/test/scala/EmployeeViewStockTest.scala
@@ -6,12 +6,7 @@ class EmployeeViewStockTest extends FlatSpec {
       val shop = new Shop
       var stock = new Stock
       var item1 = shop.defineAnItem(ItemTypes.Game, "Call of Duty",2.50, 5)
-      stock.productQuantity(item1.getItemID())
-
-
-      (assert(stock.getAmountOfProductsForThisID(item1)Stock.productQuantity)
-
+      stock.updateStockForID(item1.getItemID(),12)
+      assert((stock.productQuantity(item1.getItemID())===12))
     }
-
-
 }
