059d7331b41e713878fa0905018172b9d0a986a8
Administrator <arkadiusk.kowalski.qa.com>
2017-06-22 11:20:03 +0100

    test for producing receipt done

--- src/test/scala/EmployeeProduceReceiptTest.scala
+++ src/test/scala/EmployeeProduceReceiptTest.scala
@@ -7,7 +7,7 @@ import java.util.Date
   * Created by Profile on 20/06/2017.
   */
 class EmployeeProduceReceiptTest extends FlatSpec {
-
+//done
  " As an Employee I" should " be able to produce a receipt for a customer" in {
    var shop = new Shop()
 
@@ -21,23 +21,8 @@ class EmployeeProduceReceiptTest extends FlatSpec {
    employee1.openShop(shop)
    val sellDetails:(Array[Item], Double, Int) = shop.sellThis(listOfItemsToBuy,itemStock)
     employee1.finaliseSale(sellDetails._1,sellDetails._2,customer,employee1,itemStock,summary,shop,Some(sellDetails._3))
-  // employee1.finaliseSale(sellDetails._1,sellDetails._2,customer,employee1,itemStock,summary,shop)
    val saleRecord = summary.getSaleRecord(customer.receivedReciptsIDs(0))
    assert(saleRecord.getCustomerID.customerId == customer.customerId)
    assert(saleRecord.getReciptId == customer.receivedReciptsIDs(0))
-
-   //val salesRecord = new SaleRecord(sellDetails._1,sellDetails.._2,today,employee1,customer,4,103)
-
-  // assert( salesRecord.purchaseList === listOfItemsToBuy)
-
     }
   }
-
-////info("So that they have a record of their purchase ")
-//info ("Produce receipt")
-//info ("Employees can produce a receipt for customers")
-//
-//Should ("The employee sell a product")  ("The Employee makes a sale")
-//
-//
-//Then ("The system prints a receipt")
