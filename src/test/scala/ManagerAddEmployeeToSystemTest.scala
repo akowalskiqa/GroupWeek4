ac96193e370c500f8df8f3f829251b49e792b782
Imgundams <Imgundams@users.noreply.github.com>
2017-06-22 10:19:57 +0100

    Dan's merge dev to main

--- src/test/scala/ManagerAddEmployeeToSystemTest.scala
+++ src/test/scala/ManagerAddEmployeeToSystemTest.scala
@@ -1,6 +1,22 @@
+import scala.collection.mutable.ArrayBuffer
+
 /**
   * Created by Profile on 22/06/2017.
   */
 class ManagerAddEmployeeToSystemTest {
+
+  "As a Manager I want to be able to add employee to the system" should "add a new employee from the system" in {
+    var shop = new Shop()
+    var employee1 = new FloorStaff("Elliot", shop.idGenerator.uniqueEmployeeId)
+    var employee2 = new FloorStaff("Dave", shop.idGenerator.uniqueEmployeeId)
+    var employeeList = ArrayBuffer(employee1,employee2) // existing list is populated
+
+    var newEmployee = new FloorStaff("NewDude", shop.idGenerator.uniqueEmployeeId)
+
+
+    employeeList += newEmployee
+
+    assert(employeeList.last == newEmployee)
+  }
 
 }
