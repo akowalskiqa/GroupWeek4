8a70f4bf836e46adcadf1982c81e483b1b09f5d2
Imgundams <Imgundams@users.noreply.github.com>
2017-06-22 10:33:25 +0100

    Dan's add employee test

--- src/test/scala/ManagerAddEmployeeToSystemTest.scala
+++ src/test/scala/ManagerAddEmployeeToSystemTest.scala
@@ -6,16 +6,8 @@ class ManagerAddEmployeeToSystemTest extends FlatSpec {
 
   "As a Manager I want to be able to add employee to the system" should "add a new employee from the system" in {
     var shop = new Shop()
-    var employee1 = new FloorStaff("Elliot", shop.idGenerator.uniqueEmployeeId)
-    var employee2 = new FloorStaff("Dave", shop.idGenerator.uniqueEmployeeId)
-    var employeeList = ArrayBuffer(employee1,employee2) // existing list is populated
-
     var newEmployee = new FloorStaff("NewDude", shop.idGenerator.uniqueEmployeeId)
-
-
-    employeeList += newEmployee
-
-    assert(employeeList.last == newEmployee)
+    shop.createAnFloorStaff(newEmployee)
+    assert(shop.listOfFLoorStaff.last === newEmployee)
   }
-
 }
