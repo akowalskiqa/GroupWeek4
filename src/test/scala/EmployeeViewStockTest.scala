853bb4519e2ab278d4d1d2d6004aeeea8ee330b8
Fi <feeon@live.com>
2017-06-22 13:53:17 +0100

    Employee Pre Order Test - completed

--- src/test/scala/EmployeeViewStockTest.scala
+++ src/test/scala/EmployeeViewStockTest.scala
@@ -3,7 +3,7 @@ import org.scalatest.FlatSpec
 class EmployeeViewStockTest extends FlatSpec {
 
     "As an employee I" can "view an items stock levels" in {
-      val shop = new Shop
+      var shop = new Shop
       var stock = new Stock
       var item1 = shop.defineAnItem(ItemTypes.Game, "Call of Duty",2.50, 5)
       stock.updateStockForID(item1.getItemID(),12)
--- src/test/scala/ManagerAddEmployeeFromSystem.scala
+++ src/test/scala/ManagerAddEmployeeFromSystem.scala
@@ -5,19 +5,14 @@ import scala.collection.mutable.ArrayBuffer
 /**
   * Created by Profile on 22/06/2017.
   */
-class ManagerDeleteEmployeeFromSystem extends FlatSpec{
+class ManagerAddEmployeeFromSystem extends FlatSpec{
 
   "As a Manager I want to be able to add a new employee to the system" should "Add a new employee" in {
     var shop = new Shop()
     var employee1 = shop.listOfFLoorStaff(1)
-
-
     var item1 = shop.defineAnItem(ItemTypes.Game, "Mass Effect", 15.0, 10)
-
     var itemlist: ArrayBuffer[Item] = ArrayBuffer()
-
     itemlist += item1
-
     assert(itemlist.last == item1)
   }
 
