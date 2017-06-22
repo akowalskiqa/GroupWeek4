f9c92f5879fd2e2eaa44c1a306eeaaaee0d05a4d
Scott <scott3933@gmail.com>
2017-06-22 10:29:45 +0100

    Even more working

--- src/test/scala/EmployeeSellProductsTest.scala
+++ src/test/scala/EmployeeSellProductsTest.scala
@@ -1,8 +1,6 @@
 // DONE this works
 import org.scalatest.FlatSpec
 
-import scala.collection.mutable.ArrayBuffer
-
 /**
   * Created by Profile on 20/06/2017.
   */
@@ -25,22 +23,27 @@ class EmployeeSellProductsTest extends FlatSpec {
     }
 
   }
+
+  //Experimental second part of test - Delete if not working
   it should "not sell a product without staff" in{
     var shop = new Shop()
+    shop.openShop()
     var item1 = shop.defineAnItem(ItemTypes.Game, "Bonestorm", 10.0, 10)
     var item2 = null
     var shoppingList = Array(item1)
     var stockList = new Stock()
-    stockList.productQuantity +=(11-> 100)
+    stockList.productQuantity +=(item1.getItemID()-> 100)
     val whoInvokedThisCall = new Customer("Richard", 1234, true, 105)
 
     if (whoInvokedThisCall.isInstanceOf[FloorStaff]) {
 
       shop.sellThis  (shoppingList, stockList)
-      assert(shoppingList.contains(item1))
+      assert(shoppingList.exists(item => item.getItemID()== item1.getItemID()))
+      //assert(shoppingList.contains(item1))
     }
     else{
-      assert(!shoppingList.contains(item2))
+      //assert(!shoppingList.contains(item2))
+      assert(!shoppingList.exists(item => item.getItemID()== item1.getItemID()))
     }
   }
 }
